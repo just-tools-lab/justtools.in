f5f5719d4e515c5d390b2c2d0c4e52d2901e4fce
harshavardhanSDE <harshavardhan.workspace@gmail.com>
2024-11-12 11:22:35 +0530

    added: react-comp: Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from "@/ui/navbar/Navbar.tsx"
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <a href="justtools.in">justtools</a>
     </>
   )
---
+++ src/ui/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar : React.FC = () => {
+    return (
+        <div>
+            <ul>
+                <li>Tools</li>
+                <li>Templates</li>
+                <li>Community</li>
+                <li>Newsletter</li>
+                <li>About</li>
+            </ul>
+        </div>
+    )
+}; 
+
+export default Navbar;
